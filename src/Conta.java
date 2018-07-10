cf5d1bba721a5fb9dbf4c287dec62540d71f3247
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2018-07-10 10:40:18 -0300

    primeiro commit

---
+++ src/Conta.java
@@ -0,0 +1,27 @@
+
+public class Conta {
+	private String numero;
+	private float saldo;
+	
+	// CONSTRUTORES
+	public Conta (String numero, float saldo)
+	{
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+	public Conta (String numero)
+	{
+		this.numero = numero;
+		this.saldo = 0;
+	}
+
+	// GETS
+	public String getNumero() {	return numero; }
+	public float getSaldo() { return saldo; }
+	
+	// CREDITAR E DEBITAR
+	public void creditar (float valor) { this.saldo += valor; }
+	public void debitar (float valor) { this.saldo -= valor; }
+	
+	
+}
