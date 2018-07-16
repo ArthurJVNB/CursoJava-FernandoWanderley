2aac17dd11327f33e1575fe3f71246a09ac76177
Euller <eullernobrega@gmail.com>
2018-07-16 16:06:58 -0300

    Cadastro de Contas - Regra de Negocio

---
+++ src/CadConta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fwinternetbanking;
+
+/**
+ *
+ * @author Euller
+ */
+public class CadConta {
+    private IRepConta contas;
+    
+        public CadConta  (IRepConta rep){
+            this.contas = rep;
+        }
+        
+        
+}
