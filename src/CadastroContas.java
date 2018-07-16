3c18dae4eed77609f5bdbc02d7fc3898f75aca85
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2018-07-16 18:31:10 -0300

    adicionados ContaImposto e CadastroContas

--- src/CadastroContas.java
+++ src/CadastroContas.java
@@ -1,4 +1,34 @@
 
 public class CadastroContas {
+	private IRepositorioContas contas;
+
+	// CONSTRUCTOR
+	public CadastroContas(IRepositorioContas contas)
+	{
+		this.contas = contas;
+	}
 	
+	// Inserir
+	public void cadastrar(ContaAbstrata conta)
+	{
+		contas.inserir(conta);
+	}
+	
+	// Remover
+	public void remover(ContaAbstrata conta)
+	{
+		contas.remover(conta);
+	}
+	
+	// Consultar
+	public ContaAbstrata consultar(String numeroConta)
+	{
+		return contas.procurar(numeroConta);
+	}
+	
+	// Atualizar
+	public void atualizar(ContaAbstrata conta)
+	{
+		contas.atualizar(conta);
+	}
 }
