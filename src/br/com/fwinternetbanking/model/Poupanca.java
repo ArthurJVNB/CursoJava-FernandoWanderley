dbc11d2a4d6ff330cf55fe4dc6a8c7dac17af44a
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2018-07-19 18:09:21 -0300

    correcao do metodo renderJuros

--- src/br/com/fwinternetbanking/model/Poupanca.java
+++ src/br/com/fwinternetbanking/model/Poupanca.java
@@ -2,7 +2,8 @@ package br.com.fwinternetbanking.model;
 
 
 public class Poupanca extends Conta {
-
+	private static double TAXA_JUROS = 0.01; // 1%
+	
     // CONSTRUCTOR 1
     public Poupanca(String numero, double saldo, Cliente cliente) {
         super(numero, saldo, cliente);
@@ -10,6 +11,6 @@ public class Poupanca extends Conta {
 
     public void renderJuros(double taxa) {
         double saldoAtual = getSaldo();
-        creditar(saldoAtual * (taxa / 100));
+        creditar(saldoAtual * TAXA_JUROS);
     }
 }
