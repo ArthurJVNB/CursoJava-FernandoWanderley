aade0879962cf723ce1a5587b33faa53f363aee4
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2018-07-24 23:31:38 -0300

    correcao de alguns warnings

--- src/br/com/fwinternetbanking/exceptions/ContaNaoEncontradaException.java
+++ src/br/com/fwinternetbanking/exceptions/ContaNaoEncontradaException.java
@@ -1,13 +1,11 @@
 package br.com.fwinternetbanking.exceptions;
 
 public class ContaNaoEncontradaException extends Exception {
-	/**
-	 * 
-	 */
 
 	/**
 	 * 
 	 */
+	private static final long serialVersionUID = 1L;
 
 	public ContaNaoEncontradaException() {
 		super("Conta nao encontrada");
