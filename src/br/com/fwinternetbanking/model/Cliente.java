a4c891527e8e25372702ed67ea639031521c90dd
Arthur Vila Nova <arthurjvnb@yahoo.com.br>
2018-07-25 17:18:27 -0300

    WIP hibernate para Cliente, Endereco e Gerente em 25.07.2018 as 17:18h

--- src/br/com/fwinternetbanking/model/Cliente.java
+++ src/br/com/fwinternetbanking/model/Cliente.java
@@ -1,5 +1,17 @@
 package br.com.fwinternetbanking.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,12 +21,31 @@ package br.com.fwinternetbanking.model;
  *
  * @author Euller
  */
+
+@Entity
+@Table (name = "TB_CONTA")
 public class Cliente implements Comparable<Cliente> {
 
+	@Id
+	@ManyToMany (targetEntity = Gerente.class)
+	@JoinTable (name = "TB_GERENTES_CLIENTE", joinColumns = {
+			@JoinColumn (name = "TB_CLIENTE_CPF")
+	},
+	inverseJoinColumns = {
+			@JoinColumn (name = "TB_GERENTE_ID")
+	})
+	@JoinColumn (name = "CPF")
+	private String cpf;
 	
-	private final String cpf;
+	@JoinColumn (name = "NOME")
 	private String nome;
+	
+	// NAO VAI SER GUARDADO!
+	// TODO talvez seja melhor retirar esse atributo ---Arthur: dia 2018.07.25, às 16:28h 
 	private TipoCliente tipo;
+	
+	@OneToOne (cascade = CascadeType.ALL)
+	@PrimaryKeyJoinColumn
 	private Endereco endereco;
 
 //	public Cliente(String nome, String cpf, TipoCliente tipo, String cep, String numero, String complemento) {
@@ -24,11 +55,12 @@ public class Cliente implements Comparable<Cliente> {
 //		this.tipo = tipo;
 //	}
 
+	public Cliente(){}
 	public Cliente(String cpf, String nome, String cep, String numero, String complemento) {
 		super();
 		this.cpf = cpf;
 		this.nome = nome;
-		this.endereco = new Endereco(cep, numero, complemento);
+		this.endereco = new Endereco(cpf, cep, numero, complemento);
 	}
 
 	public String getCpf() {
